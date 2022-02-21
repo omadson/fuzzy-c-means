aa19e5532e6012c4687e6d948d294500399c3a19
Madson Dias <madsonddias@gmail.com>
2022-02-21 15:19:30 -0300

    docs(main): adds docstrings.

--- fcmeans/main.py
+++ fcmeans/main.py
@@ -1,12 +1,48 @@
 from typing import Optional
 
 import numpy as np
+from numpy.typing import NDArray
 from pydantic import BaseModel, Extra, Field, validate_arguments
-
-from .my_typing import ArrayLike
 
 
 class FCM(BaseModel):
+    r"""Fuzzy C-means Model
+
+    Parameters
+    ----------
+    n_clusters : int
+        The number of clusters to form as well as the number of centroids
+        to generate by the fuzzy C-means.
+    max_iter : int
+        Maximum number of iterations of the fuzzy C-means algorithm
+        for a single run.
+    m : float
+        Degree of fuzziness $m \in (1, infty)$.
+    error : float
+        Relative tolerance with regards to Frobenius norm of the difference
+        in the cluster centers of two consecutive iterations to declare
+        convergence.
+    random_state : Optional[int]
+        Determines random number generation for centroid initialization.
+        Use an int to make the randomness deterministic.
+    trained : bool
+        Variable to store whether or not the model has been trained.
+
+    Returns
+    -------
+    FCM
+        A FCM model.
+
+    Raises
+    ------
+    ReferenceError
+        If called without the model being trained
+    """
+
+    class Config:
+        extra = Extra.allow
+        arbitrary_types_allowed = True
+
     n_clusters: int = Field(5, ge=1, le=100)
     max_iter: int = Field(150, ge=1, le=1000)
     m: float = Field(2.0, ge=1.0)
@@ -14,12 +50,8 @@ class FCM(BaseModel):
     random_state: Optional[int] = None
     trained: bool = Field(False, const=True)
 
-    class Config:
-        extra = Extra.allow
-        arbitrary_types_allowed = True
-
-    @validate_arguments
-    def fit(self, X: ArrayLike) -> None:
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def fit(self, X: NDArray) -> None:
         """Train the fuzzy-c-means model..
 
         Parameters
@@ -42,64 +74,74 @@ class FCM(BaseModel):
                 break
         self.trained = True
 
-    def soft_predict(self, X: ArrayLike) -> ArrayLike:
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def soft_predict(self, X: NDArray) -> NDArray:
         """Soft predict of FCM
 
         Parameters
         ----------
-        X : array, shape = [n_samples, n_features]
+        X : NDArray
             New data to predict.
 
         Returns
         -------
-        array, shape = [n_samples, n_clusters]
+        NDArray
             Fuzzy partition array, returned as an array with n_samples rows
             and n_clusters columns.
         """
-        temp = FCM._dist(X, self._centers) ** float(2 / (self.m - 1))
+
+        temp = FCM._dist(X, self._centers) ** (2 / (self.m - 1))
         denominator_ = temp.reshape((X.shape[0], 1, -1)).repeat(
             temp.shape[-1], axis=1
         )
         denominator_ = temp[:, :, np.newaxis] / denominator_
         return 1 / denominator_.sum(2)
 
-    @validate_arguments
-    def predict(self, X: ArrayLike):
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def predict(self, X: NDArray) -> NDArray:
         """Predict the closest cluster each sample in X belongs to.
 
         Parameters
         ----------
-        X : array, shape = [n_samples, n_features]
+        X : NDArray
             New data to predict.
 
         Returns
         -------
-        labels : array, shape = [n_samples,]
+        NDArray
             Index of the cluster each sample belongs to.
+
+        Raises
+        ------
+        ReferenceError
+            If called without the model being trained
         """
-        if self.is_trained():
+        if self._is_trained():
             X = np.expand_dims(X, axis=0) if len(X.shape) == 1 else X
             return self.soft_predict(X).argmax(axis=-1)
+        raise ReferenceError(
+            "You need to train the model. Run `.fit()` method to this."
+        )
 
-    def is_trained(self) -> bool:
+    def _is_trained(self) -> bool:
         if self.trained:
             return True
         return False
 
     @staticmethod
-    def _dist(A: ArrayLike, B: ArrayLike):
+    def _dist(A: NDArray, B: NDArray) -> NDArray:
         """Compute the euclidean distance two matrices"""
         return np.sqrt(np.einsum("ijk->ij", (A[:, None, :] - B) ** 2))
 
     @staticmethod
-    def _next_centers(X, u, m):
+    def _next_centers(X: NDArray, u: NDArray, m: float):
         """Update cluster centers"""
         um = u**m
         return (X.T @ um / np.sum(um, axis=0)).T
 
     @property
-    def centers(self):
-        if self.is_trained():
+    def centers(self) -> NDArray:
+        if self._is_trained():
             return self._centers
         raise ReferenceError(
             "You need to train the model. Run `.fit()` method to this."
@@ -108,14 +150,20 @@ class FCM(BaseModel):
     @property
     def partition_coefficient(self) -> float:
         """Partition coefficient
-        (Equation 12a of https://doi.org/10.1016/0098-3004(84)90020-7)
+
+        Equation 12a of https://doi.org/10.1016/0098-3004(84)90020-7
 
         Returns
         -------
         float
-            partition coefficient of clustering model
+            Partition coefficient of clustering model
+
+        Raises
+        ------
+        ReferenceError
+            If called without the model being trained
         """
-        if self.is_trained():
+        if self._is_trained():
             return np.mean(self.u**2)
         raise ReferenceError(
             "You need to train the model. Run `.fit()` method to this."
@@ -123,7 +171,7 @@ class FCM(BaseModel):
 
     @property
     def partition_entropy_coefficient(self):
-        if self.is_trained():
+        if self._is_trained():
             return -np.mean(self.u * np.log2(self.u))
         raise ReferenceError(
             "You need to train the model. Run `.fit()` method to this."
